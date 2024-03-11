6cb37902c88f9c6c7463402237593042202bdc22
Borisko47 <zvyagincev.boris@yandex.ru>
2024-03-12 01:26:26 +0300

    add_logging

---
+++ NewsPaper/simpleapp/filters.py
@@ -0,0 +1,22 @@
+from django_filters import FilterSet, ModelChoiceFilter
+from .models import Product, Material
+
+
+class ProductFilter(FilterSet):
+    material = ModelChoiceFilter(
+        field_name='productmaterial__material',
+        queryset=Material.objects.all(),
+        label='Material',
+        empty_label='Выберите материал',
+    )
+
+    class Meta:
+        model = Product
+        fields = {
+            'name': ['icontains'],
+            'quantity': ['gt'],
+            'price': [
+                'lt',
+                'gt',
+            ]
+        }
